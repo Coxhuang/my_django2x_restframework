10142a980dd11e3d9a51ecdf216469c45b9b14b8
Coxhuang <mhesat@outlook.com>
2020-03-29 22:55:59 +0800

    update

---
+++ basedata/target/init_data.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2020/3/29 18:30
+@Auth ： Minhang
+@File ：init_data.py
+@IDE  ：PyCharm
+"""
---
+++ my_django2x_restframework/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@Time ： 2020/3/29 10:43
+@Auth ： Minhang
+@File ：config.py
+@IDE  ：PyCharm
+"""
+
+
+
