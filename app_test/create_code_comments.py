d7d65210198b67ae4e35fa9283e55ad319b2f506
Coxhuang <mhesat@outlook.com>
2020-03-29 09:32:04 +0800

    update

--- app_test/create_code_comments.py
+++ app_test/create_code_comments.py
@@ -13,11 +13,11 @@ class CodeComments(object):
     """
 
     def __init__(self):
-        self.title = "# * @标题 : "
-        self.function = "# * @功能 : "
-        self.remark = "# * @备注 : "
-        self.margin = "# *********************************************************************"
-        self.padding = "# *                                                                   *"
+        self.title = "# * @标题  : "
+        self.function = "# * @功能  : "
+        self.remark = "# * @备注  : "
+        self.margin = "# *******************************************************************************"
+        self.padding = "# *                                                                             *"
         self.content_list = []
 
     def create_tools_comments(self):
@@ -28,30 +28,47 @@ class CodeComments(object):
         self.title += input("请输入 标题 : ")
         self.function += input("请输入 功能 : ")
         self.remark += input("请输入 备注 : ")
+        count = 1
         while True:
-            content = input("请输入 说明 : ")
+            content = input("请输入 说明(输入0退出) : ")
             if content == "0" :
                 break
-            self.content_list.append(content)
+            print_str = "# * #说明{} : {}".format(count + 1, content)
+            self.content_list.append(print_str)
+
+
 
         print(self.margin)
         print(self.padding)
         print(self.title)
         print(self.function)
         print(self.remark)
-        for index, foo in enumerate(self.content_list):
-            print("# * #说明{} : {}".format(index+1, foo))
+
+        for foo in self.content_list:
+            print(foo)
+
 
         print(self.padding)
         print(self.margin)
 
         return None
+"""
+# *********************************************************************
+# *                                                                   *
+# * @标题  : 跨域配置
+# * @功能  : 解决前后端分离跨域问题
+# * @备注  : 看一下说明,有几个需要注意的地方
+# * #说明1 : 在settings.py文件中添加中间件 corsheaders.middleware.CorsMiddleware
+# * #说明2 : 中间件corsheaders.middleware.CorsMiddleware必须放在第三的位置,不能任意放
+# *                                                                   *
+# *********************************************************************
+"""
 
-    def create_code_comments(self):
-        """
-        创建code注释
-        :return: string
-        """
+    # def create_code_comments(self):
+    #     """
+    #     创建code注释
+    #     :return: string
+    #     """
 
 
 if __name__ == "__main__":
