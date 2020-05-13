f636a2303f99d9efcdb0ac431e7f6895224a53b6
Coxhuang <mhesat@outlook.com>
2020-05-13 11:13:13 +0800

    add unittest

--- app_test/tests.py
+++ app_test/tests.py
@@ -1,26 +1,58 @@
-# from django.test import TestCase
+from django.test import TestCase,Client
+from rest_framework.test import APITestCase
+from app_user import models
 
-# Create your tests here.
-# import time
-# import eventlet#导入eventlet这个模块
-# eventlet.monkey_patch()#必须加这条代码
-# with eventlet.Timeout(2,False):#设置超时时间为2秒
-#    time.sleep(1)
-#    print('没有跳过这条输出')
-# print('跳过了输出')
+class AppUserTest(TestCase):
 
-import time
-import timeout_decorator
-import random
+    def setUp(self):
+        """
+        每个测试用例执行之前做操作
+        :return:
+        """
+        print('每个测试用例执行之前做操作')
 
-@timeout_decorator.timeout(5)
-def mytest():
-   a = random.randint(1,5)
-   time.sleep(a)
-   return a
-while 1:
-   try:
-      print(mytest())
-   except:
-      print("超时退出")
-      break
+    def tearDown(self):
+        """
+        每个测试用例执行之后做操作
+        :return:
+        """
+        print('每个测试用例执行之后做操作')
+
+    @classmethod
+    def tearDownClass(self):
+        """
+        所有test运行完后运行一次
+        :return:
+        """
+        print('所有test运行完后运行一次')
+
+    @classmethod
+    def setUpClass(self):
+        """
+        所有test运行前运行一次
+        :return:
+        """
+        print('所有test运行前运行一次')
+
+    # def test_user_register(self):
+    #     """
+    #     单元测试方法, 只要满足以test开头的函数名即是 单元测试方法
+    #     :return:
+    #     """
+    #     res = models.UserProfile.objects.create_user(
+    #         username='xxx',
+    #         password='xxx',
+    #     )
+    #     # self.assertEqual(res.email,"lihuacai168@gmail.com")
+    #     print("单元测试方法")
+
+    def test_api(self):
+        """
+        单元测试方法, 只要满足以test开头的函数名即是 单元测试方法
+        :return:
+        """
+        response = self.client.get('/api/car/get-car-profile/')
+        print(response.data)
+        # print(dir(response))
+        self.assertEquals(response.status_code, 200)
+        print("单元测试方法")
