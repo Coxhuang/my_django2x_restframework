4c6d936a7814a26f6797e42d511725d5a9b61a6a
huangminhang <mhesat@outlook.com>
2020-05-27 00:02:10 +0800

    update

--- utils/common/signals/signal.py
+++ utils/common/signals/signal.py
@@ -1,19 +1,17 @@
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, post_save
-from django.db.backends.signals import connection_created
-from django.db.models.signals import pre_migrate, post_migrate
-from django.core.signals import request_finished
-from django.core.signals import request_started
-from django.core.signals import got_request_exception
-from django.db.models.signals import class_prepared
-from django.db.models.signals import pre_init, post_init
-from django.db.models.signals import pre_save, post_save
-from django.db.models.signals import pre_delete, post_delete
-from django.db.models.signals import m2m_changed
-from django.db.models.signals import pre_migrate, post_migrate
-from django.test.signals import setting_changed
-from django.test.signals import template_rendered
-from django.db.backends.signals import connection_created
+from django.db.models.signals import pre_save, post_save # django的modal对象保存前/后，自动触发
+from django.db.backends.signals import connection_created # 创建数据库连接时，自动触发
+from django.db.models.signals import pre_migrate, post_migrate # 执行migrate命令前/后，自动触发
+from django.core.signals import request_finished # 请求结束后，自动触发
+from django.core.signals import request_started # 请求到来前，自动触发
+from django.core.signals import got_request_exception # 请求异常后，自动触发
+from django.db.models.signals import class_prepared # 程序启动时，检测已注册的app中modal类，对于每一个类，自动触发
+from django.db.models.signals import pre_init, post_init  # django的modal执行其构造方法前/后，自动触发
+from django.db.models.signals import pre_save, post_save # django的modal对象保存前/后，自动触发
+from django.db.models.signals import pre_delete, post_delete # django的modal对象删除前/后，自动触发
+from django.db.models.signals import m2m_changed # django的modal中使用m2m字段操作第三张表（add,remove,clear）前后，自动触发
+from django.test.signals import setting_changed # 使用test测试修改配置文件时，自动触发
+from django.test.signals import template_rendered # 使用test测试渲染模板时，自动触发
 from app_user.models import UserProfile
 from data.global_enums import GetUserConfig
 from utils.common.logs.log import LogsBase
